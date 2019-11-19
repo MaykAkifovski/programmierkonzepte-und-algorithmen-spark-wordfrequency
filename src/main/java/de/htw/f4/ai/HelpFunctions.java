00b6c9324793b593e4bcc0266a4739cea545b6fd
mayk.akifovski <mayk.akifovski@gmail.com>
2019-11-19 16:38:28 +0100

    first commit

---
+++ src/main/java/de/htw/f4/ai/HelpFunctions.java
@@ -0,0 +1,57 @@
+package de.htw.f4.ai;
+
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import scala.Tuple2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.Collections.emptyList;
+import static java.util.Objects.requireNonNull;
+import static org.apache.commons.lang3.StringUtils.SPACE;
+
+public class HelpFunctions {
+
+    private List<String> stopwords;
+
+    public HelpFunctions() throws URISyntaxException, IOException {
+        Path path = Paths.get(Thread.currentThread().getContextClassLoader().getResource("stopwords.txt").toURI());
+        try (Stream<String> lines = Files.lines(path)) {
+            this.stopwords = lines.collect(Collectors.toList());
+            this.stopwords.add("");
+        }
+    }
+
+    public JavaRDD<String> splitAndCleanLines(JavaRDD<String> rddLines) {
+        List<String> _stopwords = this.stopwords;
+        return rddLines
+                .map(line -> line.split("\\W+"))
+                .map(Arrays::asList)
+                .flatMap(List::iterator)
+                .map(String::toLowerCase)
+                .filter(word -> !_stopwords.contains(word));
+
+    }
+
+    public JavaRDD<Tuple2<String, Integer>> countWords(JavaRDD<String> rddWords) {
+        return rddWords
+                .mapToPair(word -> new Tuple2<>(word, 1))
+                .reduceByKey(Integer::sum)
+                .map(t -> t)
+                .sortBy(pair -> pair._2, false, 1);
+    }
+}
---
+++ src/main/java/de/htw/f4/ai/Main.java
@@ -0,0 +1,45 @@
+package de.htw.f4.ai;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
+import scala.Tuple3;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.List;
+
+public class Main {
+
+    private static final String FILE = Thread.currentThread().getContextClassLoader().getResource("The_Adventures_of_Tom_Sawyer.txt").getPath();
+    private static final String FILE_2 = Thread.currentThread().getContextClassLoader().getResource("The_Adventures_of_Tom_Sawyer_2.txt").getPath();
+    private static final String FILE_4 = Thread.currentThread().getContextClassLoader().getResource("The_Adventures_of_Tom_Sawyer_4.txt").getPath();
+    private static final String FILE_ALL = Thread.currentThread().getContextClassLoader().getResource("all_test_files.txt").getPath();
+
+    public static void main(String[] args) throws IOException, URISyntaxException {
+        SparkConf sparkConf = new SparkConf().setAppName("JavaWordCount").setMaster("local[*]");
+        try (JavaSparkContext jsc = new JavaSparkContext(sparkConf)) {
+            HelpFunctions helpFunctions = new HelpFunctions();
+
+            Tuple3<List<Tuple2<String, Integer>>, Long, Long> simple_corpus = countTop10WordsAndTime(FILE, jsc, helpFunctions);
+            Tuple3<List<Tuple2<String, Integer>>, Long, Long> double_corpus = countTop10WordsAndTime(FILE_2, jsc, helpFunctions);
+            Tuple3<List<Tuple2<String, Integer>>, Long, Long> quadratic_corpus = countTop10WordsAndTime(FILE_4, jsc, helpFunctions);
+            Tuple3<List<Tuple2<String, Integer>>, Long, Long> all_texts = countTop10WordsAndTime(FILE_ALL, jsc, helpFunctions);
+
+
+            jsc.stop();
+        }
+    }
+
+    private static Tuple3<List<Tuple2<String, Integer>>, Long, Long> countTop10WordsAndTime(String file, JavaSparkContext jsc, HelpFunctions helpFunctions) {
+        long startTime = System.nanoTime();
+        JavaRDD<String> rddLines = jsc.textFile(file, 4);
+        JavaRDD<String> rddWords = helpFunctions.splitAndCleanLines(rddLines);
+        JavaRDD<Tuple2<String, Integer>> wordsFreqSortedRDD = helpFunctions.countWords(rddWords);
+        List<Tuple2<String, Integer>> wordsFreqSorted = wordsFreqSortedRDD.collect();
+        long endTime = System.nanoTime();
+        long totalTime = (endTime - startTime) / 1000;
+        return new Tuple3<>(wordsFreqSorted.subList(0, 10), rddWords.count(), totalTime);
+    }
+}
