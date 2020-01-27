729f7a256760813eaf91171c9adbc489f53d0b3c
Sopranoe <s0544642@htw-berlin.de>
2020-01-27 21:22:16 +0100

    Added variables and prints

--- src/main/java/de/htw/f4/ai/Main.java
+++ src/main/java/de/htw/f4/ai/Main.java
@@ -23,40 +23,55 @@ public class Main {
         SparkConf sparkConf = new SparkConf().setAppName("JavaWordCount").setMaster("local[*]");
         try (JavaSparkContext jsc = new JavaSparkContext(sparkConf)) {
 
-            Result dumy = WordFrequencySpark.run(FILE, 2, jsc);
+            Result dummy = WordFrequencySpark.run(FILE, 2, jsc);
 
             // (<Top10Words and Count>, WordsCountInCorpus, TimeToCompute)
-            Result result = WordFrequencySpark.run(FILE, 2, jsc);
-            WordFrequencySpark.run(FILE, 4, jsc);
-            WordFrequencySpark.run(FILE, 8, jsc);
-            WordFrequencySpark.run(FILE, 16, jsc);
+            Result resultBooks1SparkPart2 = WordFrequencySpark.run(FILE, 2, jsc);
+            Result resultBooks1SparkPart4 = WordFrequencySpark.run(FILE, 4, jsc);
+            Result resultBooks1SparkPart8 = WordFrequencySpark.run(FILE, 8, jsc);
+            Result resultBooks1SparkPart16 = WordFrequencySpark.run(FILE, 16, jsc);
+            Result resultBooks1SparkPart32 = WordFrequencySpark.run(FILE, 32, jsc);
 
-            WordFrequencySpark.run(FILE_2, 2, jsc);
-            WordFrequencySpark.run(FILE_2, 4, jsc);
-            WordFrequencySpark.run(FILE_2, 8, jsc);
-            WordFrequencySpark.run(FILE_2, 16, jsc);
+            Result resultBooks2SparkPart2 = WordFrequencySpark.run(FILE_2, 2, jsc);
+            Result resultBooks2SparkPart4 = WordFrequencySpark.run(FILE_2, 4, jsc);
+            Result resultBooks2SparkPart8 = WordFrequencySpark.run(FILE_2, 8, jsc);
+            Result resultBooks2SparkPart16 = WordFrequencySpark.run(FILE_2, 16, jsc);
+            Result resultBooks2SparkPart32 =  WordFrequencySpark.run(FILE_2, 32, jsc);
 
-            WordFrequencySpark.run(FILE_4, 2, jsc);
-            WordFrequencySpark.run(FILE_4, 4, jsc);
-            WordFrequencySpark.run(FILE_4, 8, jsc);
-            WordFrequencySpark.run(FILE_4, 16, jsc);
+            Result resultBooks4SparkPart2 = WordFrequencySpark.run(FILE_4, 2, jsc);
+            Result resultBooks4SparkPart4 = WordFrequencySpark.run(FILE_4, 4, jsc);
+            Result resultBooks4SparkPart8 = WordFrequencySpark.run(FILE_4, 8, jsc);
+            Result resultBooks4SparkPart16 = WordFrequencySpark.run(FILE_4, 16, jsc);
+            Result resultBooks4SparkPart32 = WordFrequencySpark.run(FILE_4, 32, jsc);
 
-            WordFrequencySpark.run(FILE_ALL, 2, jsc);
-            WordFrequencySpark.run(FILE_ALL, 4, jsc);
-            WordFrequencySpark.run(FILE_ALL, 8, jsc);
-            WordFrequencySpark.run(FILE_ALL, 16, jsc);
+            Result resultBooks52SparkPart2  = WordFrequencySpark.run(FILE_ALL, 2, jsc);
+            Result resultBooks52SparkPart4  = WordFrequencySpark.run(FILE_ALL, 4, jsc);
+            Result resultBooks52SparkPart8  = WordFrequencySpark.run(FILE_ALL, 8, jsc);
+            Result resultBooks52SparkPart16 = WordFrequencySpark.run(FILE_ALL, 16, jsc);
+            Result resultBooks52SparkPart32 = WordFrequencySpark.run(FILE_ALL, 32, jsc);
+
+            System.out.println("top 10 words in 52 english books: " + resultBooks52SparkPart8.top10Words);
+            System.out.println("total number of words: " + resultBooks52SparkPart8.wordsCountInCorpus + "");
+            System.out.println("compute duration (Spark - 2 partitions): " + resultBooks52SparkPart2.timeToComputeMs + " ms");
+            System.out.println("compute duration (Spark - 4 partitions): " + resultBooks52SparkPart4.timeToComputeMs + " ms");
+            System.out.println("compute duration (Spark - 8 partitions): " + resultBooks52SparkPart8.timeToComputeMs + " ms");
+            System.out.println("compute duration (Spark - 16 partitions): " + resultBooks52SparkPart16.timeToComputeMs + " ms");
+            System.out.println("compute duration (Spark - 32 partitions): " + resultBooks52SparkPart32.timeToComputeMs + " ms");
 
             jsc.stop();
         }
 
-        WordFrequencyJava.run(FILE);
-        WordFrequencyJava.run(FILE_2);
-        WordFrequencyJava.run(FILE_4);
-        WordFrequencyJava.run(FILE_ALL);
+        Result resultBooks1Java = WordFrequencyJava.run(FILE);
+        Result resultBooks2Java = WordFrequencyJava.run(FILE_2);
+        Result resultBooks4Java = WordFrequencyJava.run(FILE_4);
+        Result resultBooks52Java = WordFrequencyJava.run(FILE_ALL);
 
-        WordFrequencyJavaParallel.run(FILE);
-        WordFrequencyJavaParallel.run(FILE_2);
-        WordFrequencyJavaParallel.run(FILE_4);
-        WordFrequencyJavaParallel.run(FILE_ALL);
+        Result resultBooks1JavaParallel = WordFrequencyJavaParallel.run(FILE);
+        Result resultBooks2JavaParallel = WordFrequencyJavaParallel.run(FILE_2);
+        Result resultBooks4JavaParallel = WordFrequencyJavaParallel.run(FILE_4);
+        Result resultBooks52JavaParallel = WordFrequencyJavaParallel.run(FILE_ALL);
+
+        System.out.println("compute duration (Java): " + resultBooks52Java.timeToComputeMs + " ms");
+        System.out.println("compute duration (Java - parallel): " + resultBooks52JavaParallel.timeToComputeMs + " ms");
     }
 }
